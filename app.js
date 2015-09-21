ee6f116a57ae44e9cb54a557769d641476725517
Jason <jason@virtru.com>
2015-09-21 18:17:34 -0500

    Update 2015-09-21T23:17:32.899Z

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-"use strict";
+// blinker
+'use strict';
 
-function noOp() {}
+var blinker = document.querySelectorAll('.blink')[0];
+var color_default = 'lime';
+
+// recursion avoids infinitely growing call stack of setInterval
+function func() {
+  var color = blinker.style.backgroundColor;
+  var isDefault = color === color_default;
+
+  blinker.style.backgroundColor = isDefault ? '' : color_default;
+  setTimeout(func, 600);
+}
+setTimeout(func, 600);
 //# sourceMappingURL=app.js.map
